91922ec309e781a3c01ee01466cec6d9be72c4b1
Ismail Ahmed <143304919+ismailAhmed0000@users.noreply.github.com>
2026-08-18 18:57:35 +0500

    mobile initial commit

---
+++ mobile-app/metro.config.js
@@ -0,0 +1,8 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { withNativeWind } = require('nativewind/metro');
+
+const config = {};
+
+module.exports = withNativeWind(mergeConfig(getDefaultConfig(__dirname), config), {
+  input: './global.css',
+});
